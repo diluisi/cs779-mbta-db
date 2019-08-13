3a5ff5d1d58f2627b968b2ca8fe8b5c3a25510ed
johnsliao <john@johnliao.org>
2019-08-12 22:06:43 -0400

    WIP

--- Oracle_Add_Indexes.sql
+++ Oracle_Add_Indexes.sql
@@ -1,12 +1,8 @@
 REM
 REM DROP INDEXES
 REM
-DROP INDEX I_MemberName;
-DROP INDEX I_PersonName;
-DROP INDEX I_DVDTitle;
+DROP INDEX i_route_id;
 REM
 REM CREATE INDEXES
 REM
-CREATE INDEX I_MemberName ON Member(MemberFirstName,MemberLastName);
-CREATE INDEX I_PersonName ON MoviePerson(PersonFirstName,PersonLastName);
-CREATE UNIQUE INDEX I_DVDTitle ON DVD(DVDTitle);
+CREATE INDEX i_route_id ON VEHICLES_HISTORY(route_id);
--- Oracle_Create_Tables.sql
+++ Oracle_Create_Tables.sql
@@ -2,6 +2,7 @@ REM
 REM DROP TABLES
 REM
 DROP TABLE VEHICLES;
+DROP TABLE VEHICLES_HISTORY;
 DROP TABLE STATUSES;
 DROP TABLE ROUTES;
 DROP TABLE DIRECTIONS;
@@ -114,4 +115,23 @@ CREATE TABLE VEHICLES(
 	CONSTRAINT vehicles_direction_id_fk FOREIGN KEY (direction_id) REFERENCES directions(direction_id),
 	CONSTRAINT vehicles_route_id_fk FOREIGN KEY (route_id) REFERENCES routes(route_id),
 	CONSTRAINT vehicles_current_status_fk FOREIGN KEY (current_status) REFERENCES statuses(status_id)
+);
+
+CREATE TABLE VEHICLES_HISTORY(
+	vehicle_id VARCHAR2(32)	NOT NULL,
+	label VARCHAR2(32),
+	bearing	NUMBER(5),
+	current_stop_sequence NUMBER(5),
+	latitude FLOAT(63)	NOT NULL,
+	longitude FLOAT(63)	NOT NULL,
+	speed NUMBER(5),
+	updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+	direction_id NUMBER(5),
+	route_id NUMBER(5),
+	current_status VARCHAR2(32),
+	stop_id NUMBER(5),
+	CONSTRAINT vehicles_history_pk PRIMARY KEY (vehicle_id),
+	CONSTRAINT vehicles_history_direction_id_fk FOREIGN KEY (direction_id) REFERENCES directions(direction_id),
+	CONSTRAINT vehicles_history_route_id_fk FOREIGN KEY (route_id) REFERENCES routes(route_id),
+	CONSTRAINT vehicles_history_current_status_fk FOREIGN KEY (current_status) REFERENCES statuses(status_id)
 );
