20e449344f8b5f8804a5a13c896a12ea1c641658
johnsliao <john@johnliao.org>
2019-08-16 12:12:45 -0400

    WIP

--- extract.py
+++ extract.py
@@ -1,40 +1,40 @@
-# Functions to return api responses as structured data
+# Dump data to csv
+import cx_Oracle
+import os
 
-import requests as r
-from mbta import *
+password = os.environ.get('ORACLE_PASSWORD')
 
+dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='orclpdb1')
+conn = cx_Oracle.connect(user='system', password=password, dsn=dsn_tns)
 
-def get_routes():
-    pass
+c = conn.cursor()
+sql = """SELECT * FROM VEHICLES_DATA_HISTORY v 
+        LEFT JOIN directions d ON d.direction_id=v.direction_id
+        LEFT JOIN routes r ON r.route_id=v.route_id 
+        LEFT JOIN statuses s ON s.status_id=v.current_status 
+        LEFT JOIN stops st ON st.stop_id=v.stop_id
+        LEFT JOIN municipalities m ON st.municipality_id=m.municipality_id
+        LEFT JOIN streets str ON str.street_id=st.on_street 
+        LEFT JOIN streets str ON str.street_id=st.at_street
+        LEFT JOIN lines l ON l.line_id=r.line_id
+        INNER JOIN colors c ON c.color_id=l.color 
+        INNER JOIN colors c ON c.color_id=l.text_color 
+        INNER JOIN colors c ON c.color_id=r.color 
+        INNER JOIN colors c ON c.color_id=r.text_color
+"""
 
+c.execute(sql)
 
-def get_schedules():
-    pass
+col_names = [row[0] for row in c.description]
 
+rows = c.fetchall()
 
-def get_stops():
-    pass
+with open('dump.csv', 'w') as fs:
+    fs.writelines(','.join(col_names))
 
+with open('dump.csv', 'a') as fs:
+    for row in rows:
+        fs.write('\n')
+        fs.writelines(','.join(['"%s"' % str(r) for r in row]))
 
-def get_trips():
-    pass
-
-
-def get_lines():
-    pass
-
-
-def get_route_patterns():
-    pass
-
-
-def get_vehicles():
-    pass
-
-
-def get_shapes():
-    pass
-
-
-def get_services():
-    pass
+conn.close()
